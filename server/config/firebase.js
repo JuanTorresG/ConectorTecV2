14bc70b3fe6591af0aab28a0472e32f4fa36ba20
JuanTorresG <juanpabloto999@gmail.com>
2024-05-07 07:21:35 -0600

    s

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -6,12 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8a0mHOOsvVuvkLD8jZyCaz3sOrnr26iM",
-  authDomain: "conectortec.firebaseapp.com",
-  projectId: "conectortec",
-  storageBucket: "conectortec.appspot.com",
-  messagingSenderId: "1030783747368",
-  appId: "1:1030783747368:web:597ee3f17cc0f96f1a3c1d"
+
 }
 
 // Usar el nombre correcto 'initializeApp' aquí
